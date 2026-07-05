e048048ad6cf8167e533754df4272fff1081a734
Shivam Patel <67837595+Shivampatel010@users.noreply.github.com>
2026-07-05 17:43:16 +0530

    Time: 410 ms (85.57%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,27 @@
+-- WITH WeatherWithLag AS (
+--     SELECT 
+--         id,
+--         recordDate,
+--         temperature,
+--         LAG(temperature, 1) OVER (order by id) AS previous_temperature,
+--         LAG(recordDate, 1) OVER (order by id) AS previous_date
+--     FROM 
+--         Weather
+-- )
+-- -- select * from WeatherWithLag
+-- SELECT 
+--     id AS ID
+-- FROM 
+--     WeatherWithLag
+-- WHERE 
+--        recordDate > previous_date And
+--     temperature > previous_temperature;
+
+-- select id as Id 
+-- from weather where temperature subdate(recordDate, interval  day) > (Select temperature from Weather where subdate(recordDate, interval 1 day))
+
+SELECT w1.id AS Id
+FROM Weather w1
+JOIN Weather w2 
+  ON w1.recordDate = SUBDATE(w2.recordDate, INTERVAL -1 DAY)
+WHERE w1.temperature > w2.temperature;
